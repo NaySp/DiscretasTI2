eb96a204f17bcebed567e660d8a7759c0670cc70
Valentina <valen201611@gmail.com>
2023-06-02 12:16:20 -0500

    Upload everything

---
+++ src/model/UnionFind.java
@@ -0,0 +1,27 @@
+package model;
+public class UnionFind {
+    private int[] parent;
+
+    public UnionFind(int size) {
+        parent = new int[size];
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); // Compresión de ruta para optimización
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            parent[rootX] = rootY; // Actualizar el padre del conjunto
+        }
+    }
+}
+
